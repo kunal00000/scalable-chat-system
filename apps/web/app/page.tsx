926e0172fb32c404b9b2a97f105eb26656f9ebae
kunal00000 <kunalverma2468@gmail.com>
2023-12-21 04:27:46 +0530

    Add message display functionality

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -4,15 +4,12 @@ import { useState } from 'react'
 import { useSocket } from '../context/SocketProvider'
 
 const Page = () => {
-  const { sendMessage } = useSocket()
+  const { sendMessage, messages } = useSocket()
 
   const [message, setMessage] = useState('')
 
   return (
     <div>
-      <div>
-        <h1>All messages will appear here</h1>
-      </div>
       <div>
         <input
           onChange={(e) => setMessage(e.target.value)}
@@ -25,6 +22,12 @@ const Page = () => {
         >
           Send
         </button>
+      </div>
+      <div>
+        <h1>All messages will appear here</h1>
+        {messages.map((msg, i) => {
+          return <p key={i}>{msg}</p>
+        })}
       </div>
     </div>
   )
